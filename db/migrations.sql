6f27f841b47ed82fad1dcb8f98c8b7dcddcd6431
Josh Hepworth <jrhepworth12@gmail.com>
2018-11-19 12:39:50 -0600

    tables created

---
+++ db/migrations.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS salty_movies;
+CREATE DATABASE salty_movies;
+
+\c salty_movies
+
+CREATE TABLE users(
+	id SERIAL PRIMARY KEY,
+	username VARCHAR(64),
+	password_digest VARCHAR(64)
+);
+
+CREATE TABLE movies(
+	id SERIAL PRIMARY KEY,
+	title VARCHAR(64),
+	description VARCHAR(200),
+	user_id INTEGER REFERENCES users(id)
+);
+
