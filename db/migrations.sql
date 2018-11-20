254808b952a2948b2ae60872bfd9ef1f4267772f
Josh Hepworth <jrhepworth12@gmail.com>
2018-11-20 10:23:56 -0600

    seed data

--- db/migrations.sql
+++ db/migrations.sql
@@ -16,3 +16,9 @@ CREATE TABLE movies(
 	user_id INTEGER REFERENCES users(id)
 );
 
+INSERT INTO movies (title, description, user_id) VALUES ('Interstellar', 'space movie about space', 1);
+INSERT INTO movies (title, description, user_id) VALUES ('Dodgeball', 'duck, dive,dip, dive, dodge', 1);
+INSERT INTO movies (title, description, user_id) VALUES ('James Bond', 'british action', 2);
+INSERT INTO movies (title, description, user_id) VALUES ('Free Willy', 'save the whales', 2);
+INSERT INTO users (username, password_digest) VALUES ('jared','123');
+INSERT INTO users (username, password_digest) VALUES ('sam','123');
